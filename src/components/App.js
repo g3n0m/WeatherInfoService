5dc7cd433d02b5216eaccd8541c25fcbdc3f2c9d
g3n0m <x9057075636@gmail.com>
2022-11-23 19:37:52 +0300

    Delete some unnecessary files and components.

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     render() {
         return (
             <>
-                <Header buttonName = {"Some button updated!!!"} />
+                <Header />
                 <Forecast />
             </>
             
